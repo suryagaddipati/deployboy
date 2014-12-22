b36ec95ce391bac352113ab21e98a5f04e365410
Surya Gaddipati <sgaddipati@groupon.com>
2014-12-22 14:26:07 -0600

    add checkout commands

--- src/main/java/com/groupon/jenkins/deployboy/DeployBoyBuildConfiguration.java
+++ src/main/java/com/groupon/jenkins/deployboy/DeployBoyBuildConfiguration.java
@@ -1,11 +1,14 @@
 package com.groupon.jenkins.deployboy;
 
-import com.groupon.jenkins.buildtype.docker.CheckoutCommands;
+import com.groupon.jenkins.buildtype.dockerimage.DockerCommandBuilder;
 import com.groupon.jenkins.buildtype.util.shell.ShellCommands;
 import com.groupon.jenkins.github.GitUrl;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
+import static com.groupon.jenkins.buildtype.dockerimage.DockerCommandBuilder.dockerCommand;
 import static java.lang.String.format;
 
 public class DeployBoyBuildConfiguration {
@@ -17,14 +20,33 @@ public class DeployBoyBuildConfiguration {
 
     public ShellCommands getShellCommands(DeploymentEventPayload payload) {
      ShellCommands    shellCommands =  new ShellCommands();
-       //shellCommands.add(getChe)
-
+        DockerCommandBuilder dockerRunCommand = dockerCommand("run")
+                .flag("rm")
+                .flag("sig-proxy=true")
+                .flag("v","/home/deploy_bot/.ssh/id_rsa:/id_rsa:ro")
+                .flag("v","/home/deploy_bot/.ssh/id_rsa_github:/id_rsa_github:ro")
+                .args(getImageName(), "sh -cx \"" + getRunCommand(payload) + "\"");
+        shellCommands.add(dockerRunCommand.get());
+        return shellCommands;
+    }
+    public ShellCommands getCheckoutCommands(DeploymentEventPayload payload){
+        ShellCommands    shellCommands =  new ShellCommands();
         GitUrl gitRepoUrl = new GitUrl(payload.getProjectUrl());
         String gitUrl = payload.getCloneUrl();
         String checkoutLocation = format("/var/%s",gitRepoUrl.getFullRepoName());
         shellCommands.add(format("git clone   %s %s",  gitUrl,checkoutLocation));
         shellCommands.add("cd " + checkoutLocation);
         shellCommands.add(format("git reset --hard  %s", payload.getRef()));
-        return shellCommands;
+        return  shellCommands;
+    }
+
+    private String getRunCommand(DeploymentEventPayload payload) {
+        Map<String,String> deploymentConfig = (Map<String, String>) config.get("deployment");
+        List<String> deployCommands = Arrays.asList(deploymentConfig.get("task"));
+        return getCheckoutCommands(payload).add(new ShellCommands(deployCommands)).toSingleShellCommand();
+    }
+
+    private String getImageName() {
+         return (String) config.get("environment_image");
     }
 }
