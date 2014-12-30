1986309f57e0d160557c1726698ae575e65e6cc3
Surya Gaddipati <surya.gaddipati@gmail.com>
2014-12-30 13:31:38 -0600

    create a buildtype

--- src/main/java/com/groupon/jenkins/deployboy/DeployRequestDotCiPlugin.java
+++ src/main/java/com/groupon/jenkins/deployboy/DeployRequestDotCiPlugin.java
@@ -7,34 +7,85 @@ import com.groupon.jenkins.util.GroovyYamlTemplateProcessor;
 import hudson.Extension;
 import hudson.Launcher;
 import hudson.model.BuildListener;
+import hudson.model.Result;
 import hudson.model.Run;
+import org.kohsuke.github.GHCommitState;
+import org.kohsuke.github.GHDeployment;
+import org.kohsuke.github.GHRepository;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Map;
+import java.util.logging.Level;
+
+import static hudson.model.Result.SUCCESS;
+import static hudson.model.Result.UNSTABLE;
 
 @Extension
 public class DeployRequestDotCiPlugin extends DotCiPluginAdapter {
-    protected DeployRequestDotCiPlugin() {
+    public DeployRequestDotCiPlugin() {
         super("deploy_request", "");
     }
 
     @Override
     public boolean perform(DynamicBuild build, Launcher launcher, BuildListener listener) {
+        build.setResult(Result.SUCCESS);
         try {
             Map<String,Object> buildEnvironment = build.getEnvironmentWithChangeSet(listener);
             Map config = new GroovyYamlTemplateProcessor(getDeployBoyYml(build), buildEnvironment).getConfig();
             DeployBoyBuildConfiguration deployBoyBuildConfiguration = new DeployBoyBuildConfiguration( config);
-            /**
-            GHDeployment deployment = repository.createDeployment("master")
+            setCommitStatus(build);
+            GHDeployment deployment = build.getGithubRepository().createDeployment("master")
                     .payload("{\"user\":\"atmos\",\"room_id\":123456}")
                     .description("question")
-                    .create(); **/
+                    .create();
             return true;
         } catch (Exception e) {
            throw new RuntimeException(e);
+        }finally {
+           setCommitStatusBackToPending(build);
         }
     }
+
+    private void setCommitStatusBackToPending(DynamicBuild build) {
+        GHRepository repository = build.getGithubRepository();
+
+        try {
+            String url = "";
+            try {
+                url = build.getFullUrl();
+            } catch (Exception e) {
+                // do nothing
+                // TODO DO SOMETHING
+            }
+            repository.createCommitStatus(build.getSha(), GHCommitState.PENDING, url, "Build in progress", "DotCi");
+        } catch (IOException e) {
+            // Ignore if cannot create a pending status
+        }
+    }
+
+    private void setCommitStatus(DynamicBuild build) {
+        GHRepository repository = build.getGithubRepository();
+        GHCommitState state;
+        String msg;
+        Result result = build.getResult();
+        if (result.isBetterOrEqualTo(SUCCESS)) {
+            state = GHCommitState.SUCCESS;
+            msg = "Success";
+        } else if (result.isBetterOrEqualTo(UNSTABLE)) {
+            state = GHCommitState.FAILURE;
+            msg = "Unstable";
+        } else {
+            state = GHCommitState.FAILURE;
+            msg = "Failed";
+        }
+        try {
+            repository.createCommitStatus(build.getSha(), state, build.getFullUrl(), msg,"DotCi");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     private String getDeployBoyYml(DynamicBuild build) throws IOException {
         try {
             return build.getGithubRepositoryService().getGHFile("deploy_boy.yml", build.getSha()).getContent();
---
+++ src/main/java/com/groupon/jenkins/deployboy/DummyBuildType.java
@@ -0,0 +1,28 @@
+package com.groupon.jenkins.deployboy;
+
+import com.groupon.jenkins.dynamic.build.DynamicBuild;
+import com.groupon.jenkins.dynamic.build.execution.BuildExecutionContext;
+import com.groupon.jenkins.dynamic.buildtype.BuildType;
+import hudson.Extension;
+import hudson.Launcher;
+import hudson.model.BuildListener;
+import hudson.model.Result;
+
+import java.io.IOException;
+
+/**
+ * Created by surya on 12/28/2014.
+ */
+@Extension
+public class DummyBuildType extends BuildType {
+    @Override
+    public String getDescription() {
+        return "Dummy";
+    }
+
+    @Override
+    public Result runBuild(DynamicBuild dynamicBuild, BuildExecutionContext buildExecutionContext, Launcher launcher, BuildListener buildListener) throws IOException, InterruptedException {
+        new DeployRequestDotCiPlugin().perform(dynamicBuild,launcher,buildListener);
+        return Result.SUCCESS;
+    }
+}
